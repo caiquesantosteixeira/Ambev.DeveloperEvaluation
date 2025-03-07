3a2cc453a01118b6a5f5fca564a8bc33f0bb3c3e
caique <caique@PROTECH-DEV-CAI>
2025-03-07 19:35:13 -0300

    ajuste iniciando contexto

--- src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
+++ src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -10,6 +10,13 @@ public class DefaultContext : DbContext
 {
     public DbSet<User> Users { get; set; }
 
+    public DbSet<BranchStore> BranchStore { get; set; }
+    public DbSet<Customer> Customer { get; set; }
+    public DbSet<Product> Product { get; set; }
+    public DbSet<Sale> Sale { get; set; }
+    public DbSet<SaleItem> SaleItem { get; set; }
+    public DbSet<Store> Store { get; set; }
+
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
     {
     }
